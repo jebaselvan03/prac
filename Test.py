24fa3071be9ee0102e609888f8c0b0675d391e96
jebaselvan03 <jebasonaselvan@gmail.com>
2026-04-13 09:10:49 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,8 +1,9 @@
 from Main1 import Add
 
 def TestAdd():
-assert Add(3, 4) == 9
-print(&quot;Add Function works correctly&quot;)
+  assert Add(3, 4) == 9
+  print(&quot;Add Function works correctly&quot;)
 
-if __name__ == &#39;__main__&#39;:
-TestAdd()
+if __name__ == '__main__':
+  TestAdd()
+
